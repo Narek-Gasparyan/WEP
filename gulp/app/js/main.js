ba7cf1b0ae833d9bcf6fe51323fc702ddb18f79d
netstalkers <netstakers@yaho.com>
2020-08-03 14:19:46 +0400

    03.08.2020

--- gulp/app/js/main.js
+++ gulp/app/js/main.js
@@ -2,7 +2,10 @@ $(function() {
 
     $('.header__slider').slick({
         infinite: true,
-        fade: true
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arroes__left" src="img/arrows-left.svg" alt=""> </img>',
+        nextArrow: '<img class="slider-arrows slider-arroes__right" src="img/arrows-right.svg" alt=""> </img>',
+
     });
 
 
