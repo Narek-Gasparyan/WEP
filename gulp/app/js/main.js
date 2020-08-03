d2c7b96f30aa7cef313c12799dd6daa96c1fc103
netstalkers <netstakers@yaho.com>
2020-08-04 02:20:23 +0400

    04.08/2:20

--- gulp/app/js/main.js
+++ gulp/app/js/main.js
@@ -5,8 +5,14 @@ $(function() {
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arroes__left" src="img/arrows-left.svg" alt=""> </img>',
         nextArrow: '<img class="slider-arrows slider-arroes__right" src="img/arrows-right.svg" alt=""> </img>',
-
+        asNavFor: '.slider-dots',
     });
+
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    })
 
 
 });
