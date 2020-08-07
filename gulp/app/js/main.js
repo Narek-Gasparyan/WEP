4819beeb44d2b183106433e950d4427a749d7f08
netstalkers <netstakers@yaho.com>
2020-08-08 02:38:31 +0400

    08.0820

--- gulp/app/js/main.js
+++ gulp/app/js/main.js
@@ -5,14 +5,27 @@ $(function() {
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arroes__left" src="img/arrows-left.svg" alt=""> </img>',
         nextArrow: '<img class="slider-arrows slider-arroes__right" src="img/arrows-right.svg" alt=""> </img>',
-        asNavFor: '.slider-dots',
+        asNavFor: '.slider-dotshead',
     });
 
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
-        asNavFor: '.header__slider',
+        asNavFor: '.slider-map',
     })
 
+    $('.sorf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arroes__left" src="img/arrows-left.svg" alt=""> </img>',
+        nextArrow: '<img class="slider-arrows slider-arroes__right" src="img/arrows-right.svg" alt=""> </img>',
+    })
 
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.sorf-slider',
+        focusOnSelect: true
+    })
 });
