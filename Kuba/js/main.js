17fa32e5888e6bb999f18171dbb6bd78b40a10ee
netstalkers <netstakers@yaho.com>
2020-08-14 04:05:43 +0400

    14.08.2020

--- Kuba/js/main.js
+++ Kuba/js/main.js
@@ -1,3 +1,8 @@
-$(function(){
+$(function() {
+
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg " alt=" "></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg " alt=" "></button>'
+    });
 
 });
