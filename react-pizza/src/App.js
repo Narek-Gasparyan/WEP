a88daf5ab52f202c72a2ddad9df97fbd8fa71294
netstalkers <netstakers@yaho.com>
2020-08-21 05:56:49 +0400

    21.08.2020

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Header from './Header';
+import { Header, Categories } from './componends';
 
 
-  
+
 
 
 function App() {
@@ -13,16 +13,10 @@ function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            <Categories
+              onClick={(name) => console.log(name)}
+              items={["Мясные", "Вегетарианская", "Гриль", "Острые", "Острые"]}
+            />
             <div className="sort">
               <div className="sort__label">
                 <svg
--- react-pizza/src/componends/Header.jsx
+++ react-pizza/src/componends/Header.jsx
@@ -1,5 +1,7 @@
 import React from  'react';
-import logoSvg from './assets/img/pizza-logo.svg';
+import logoSvg from '../assets/img/pizza-logo.svg';
+import Button from './Button';
+
 
 function Header() {
   return (
@@ -13,7 +15,7 @@ function Header() {
           </div>
         </div>
         <div className="header__cart">
-          <a href="/cart.html" className="button button--cart">
+          <Button className="button--cart">
             <span>520 ₽</span>
             <div className="button__delimiter"></div>
             <svg
@@ -21,7 +23,8 @@ function Header() {
               height="18"
               viewBox="0 0 18 18"
               fill="none"
-              xmlns="http://www.w3.org/2000/svg">              
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 d="M6.33333 16.3333C7.06971 16.3333 7.66667 15.7364 7.66667 15C7.66667 14.2636 7.06971 13.6667 6.33333 13.6667C5.59695 13.6667 5 14.2636 5 15C5 15.7364 5.59695 16.3333 6.33333 16.3333Z"
                 stroke="white"
@@ -45,7 +48,7 @@ function Header() {
               />
             </svg>
             <span>3</span>
-          </a>
+          </Button>
         </div>
       </div>
     </div>
